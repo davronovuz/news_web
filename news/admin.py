89fe89fde16348a3dbb55f635765b6c5dd5dd6a7
davronovuz <davronovtatu@gmail.com>
2025-04-17 19:01:18 +0500

    created news app and  models

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import New,Category,Comment
 
-# Register your models here.
+
+@admin.register(New)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title","category","status","published_at")
+    list_filter = ("status","category")
+    search_fields = ("title","body")
+    prepopulated_fields = {"slug":("title",)}
+
+
+
+admin.site.register(Category)
